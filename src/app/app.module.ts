d0a15a7783246ec375017b054ea7a6e627189699
Abdallah <bratosab@gmail.com>
2024-01-21 23:28:44 +0100

    06 upgrade services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UpgradeModule } from '@angular/upgrade/static';
+import { HttpClientModule } from '@angular/common/http';
+import { Phone } from './core/phone/phone.service';
+import { PhoneListComponent } from './phone-list/phone-list.component';
+import { PhoneDetailComponent } from './phone-detail/phone-detail.component';
+import { CheckmarkPipe } from './core/checkmark/checkmark.pipe';
+import { SearchPipe } from './pipes/search.pipe';
+import { OrderByPipe } from './pipes/order-by.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhoneListComponent,
+    PhoneDetailComponent,
+    CheckmarkPipe,
+    SearchPipe,
+    OrderByPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    UpgradeModule
+    UpgradeModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [Phone],
   // bootstrap: [AppComponent]
 })
 export class AppModule {
